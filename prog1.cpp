abe3078fbbc73c8c553b9bcd784079dc93a470e2
Sai Sanvi K <111369286+sanvikarthik@users.noreply.github.com>
2024-08-04 16:11:19 +0530

    Update prog1.cpp

--- prog1.cpp
+++ prog1.cpp
@@ -1,68 +1,67 @@
+#include <stdio.h>
+#include<math.h>
 #include <GL/glut.h>
-#include <cmath>
 
-void setPixel(int x, int y) {
-    glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glEnd();
+void myinit()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 500, 0, 500);
+	glMatrixMode(GL_MODELVIEW);
+
 }
-
-void lineBres(int x0, int y0, int xEnd, int yEnd) {
-    int dx = fabs(xEnd - x0), dy = fabs(yEnd - y0);
-    int p = 2 * dy - dx;
-    int twoDy = 2 * dy, twoDyMinusDx = 2 * (dy - dx);
-    int x, y;
-
-    if (x0 > xEnd) {
-        x = xEnd;
-        y = yEnd;
-        xEnd = x0;
-    }
-    else {
-        x = x0;
-        y = y0;
-    }
-
-    setPixel(x, y);
-    while (x < xEnd) {
-        x++;
-        if (p < 0)
-            p += twoDy;
-        else {
-            y++;
-            p += twoDyMinusDx;
-        }
-        setPixel(x, y);
-    }
+void setPixel(int x, int y)
+{
+	glColor3f(1, 1, 0);
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+	glFlush();
 }
-
-void display() {
-    glClearColor(0.0, 0.0, 0.0, 0.0); // Black background
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0); // White color
-
-    // Define line coordinates
-    int x0 = 50, y0 = 100;
-    int xEnd = 200, yEnd = 300;
-
-    lineBres(x0, y0, xEnd, yEnd);
-    glFlush();
+void lineBres(int x0, int y0, int xend, int yend)
+{
+	int dx = fabs(xend - x0), dy = fabs(yend - y0);
+	int p = 2 * dy - dx;
+	int twody = 2 * dy, twodyminusdx = 2 * (dy - dx);
+	int	x,y;
+	if (x0 > xend)
+	{
+		x = xend;
+		y = yend;
+		xend = x0;
+	}
+	else
+	{
+		x = x0;
+		y = y0;
+	}
+	setPixel(x, y);
+	while (x < xend)
+	{
+		x++;
+		if (p < 0) {
+			p += twody;
+		}
+		else {
+			y++;
+			p += twodyminusdx;
+		}
+		setPixel(x, y);
+	}
 }
-
-void init() {
-
-    //glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0.0, 500.0, 0.0, 500.0); // Set the window coordinates
+void display()
+{
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	lineBres(50, 100, 200, 300);
 }
-
-int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500); // Set the window size
-    glutInitWindowPosition(100, 100); // Set the window position
-    glutCreateWindow("Bresenham Line Drawing"); // Create the window
-    init();
-    glutDisplayFunc(display); // Register the display function
-    glutMainLoop();
-    return 0;
+void main()
+{
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 10);
+	glutCreateWindow("first");
+	myinit();
+	glutDisplayFunc(display);
+	glutMainLoop();
 }
